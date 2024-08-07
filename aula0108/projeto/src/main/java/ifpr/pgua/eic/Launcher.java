9e5f9b89bd55454a3cad9728371fb8fa38c50597
Giovana <gioovana.23@hotmail.com>
2024-08-07 08:44:44 -0300

    classes criadas

---
+++ aula0108/projeto/src/main/java/ifpr/pgua/eic/Launcher.java
@@ -0,0 +1,7 @@
+package ifpr.pgua.eic;
+
+public class Launcher {
+    public static void main(String[] args) {
+        App.main(args);
+    }
+}
---
+++ aula0108/projeto/src/main/java/ifpr/pgua/eic/Questao.java
@@ -0,0 +1,5 @@
+package ifpr.pgua.eic;
+
+public class Questao {
+    
+}
